574a802dae3d3f7cce30ab40c9e131182433160f
Boris Chen <boris097156@gmail.com>
2018-05-23 15:26:30 +0800

    train_op

--- depthoptic_multi_model.py
+++ depthoptic_multi_model.py
@@ -244,7 +244,9 @@ class Model(object):
             self.depth2_loss              = self.pyramid_loss(self.pre_depth2_pyramid, self.gt_depth2_pyramid)
             self.depth_loss               = self.depth1_loss + self.depth2_loss
             self.optic_loss               = self.pyramid_loss(self.pre_optic_pyramid, self.gt_optic_pyramid)
-            self.total_loss                   = self.depth_loss + self.depth_loss
+            self.total_loss               = self.depth_loss + self.depth_loss
+
+        self.train_op = tf.train.AdamOptimizer(1e-4).minimize(total_loss)
             
     def build_errors(self):
         with tf.variable_scope('errors', reuse=self.reuse_variables):
