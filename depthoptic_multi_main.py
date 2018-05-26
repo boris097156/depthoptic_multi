83b06448cb83ee3dabb58194a8a4dfd1c9863d40
Boris Chen <boris097156@gmail.com>
2018-05-26 22:27:22 +0800

    saver

--- depthoptic_multi_main.py
+++ depthoptic_multi_main.py
@@ -123,9 +123,9 @@ def test():
     coordinator = tf.train.Coordinator()
     threads     = tf.train.start_queue_runners(sess=sess, coord=coordinator)
     
-    saved_model = '{}{}'.format(FLAGS.log_directory, FLAGS.model_name)
-    print('Restore weights from {}'.format(saved_model))
-    saver.restore(sess, "{}/checkpoint".format(saved_model))
+    saved_model_dir = '{}{}'.format(FLAGS.log_directory, FLAGS.model_name)
+    print('Restore weights from {}'.format(saved_model_dir))
+    saver.restore(sess, "{}/{}".format(saved_model_dir, FLAGS.model_name))
     '''
     for net in [FLAGS.load_network]:
         print("Load network : " + net)
